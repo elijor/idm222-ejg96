22b7c1a8e3af69ce9c3f2f22bef340f443764049
elijor <ejg96@drexel.edu>
2023-02-11 15:11:54 -0500

    Adjusting spacing and nav

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuexit = document.getElementsById("toggleclose");
+// const menuopen = document.getElementById("toggleopen");
+// const menu = document.getElementsByTagName("nav");
+
+
+
+function togglenavmenu() {
+    menuexit.classList.toggle("visible");
+    console.log("hello");
+};
+
+togglenavmenu();
